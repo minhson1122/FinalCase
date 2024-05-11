21693f3db70ae85cad55b55197c3596ebf5621bc
MinhHieu3 <hieubyi@gmail.com>
2024-05-11 21:52:12 +0700

    done v1

--- src/main/java/com/example/jwtspring3/model/Album.java
+++ src/main/java/com/example/jwtspring3/model/Album.java
@@ -1,9 +1,6 @@
 package com.example.jwtspring3.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Album {
